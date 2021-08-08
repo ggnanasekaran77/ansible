5ecf5d8a48268ce27f29f2c488e630a180e63a54
Gajendiran, Gnanasekaran | RASIA <gnanasekaran.gajendi@rakuten.com>
2021-08-08 13:05:40 +0800

    playbook

--- ansible.sh
+++ ansible.sh
@@ -5,6 +5,11 @@ then
   find playbooks/${playbookPath}/ -maxdepth 1 -type f -exec cp {} . \;
 fi
 
+if [ -f "inventories/${JOB_NAME}/${targetEnv}"]
+then
+  export extraVars = "-i inventories/${JOB_NAME}/${targetEnv} ${extraVars}"
+fi
+
 
 echo "[DEVOPS-PIPE-INFO] ansible-playbook ${extraVars} ${playbookName}"
 ansible-playbook ${extraVars} ${playbookName}
